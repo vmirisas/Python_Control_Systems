eae8d6d2333fae1a4ff72bf18eec37f524860f70
Vasilis Mirisas <vmirisas@gmail.com>
2021-02-20 13:32:10 +0200

    corrections lab2 triangle generator wave name

--- lab2/signal_generators/signal_generator_triangle.py
+++ lab2/signal_generators/signal_generator_triangle.py
@@ -6,8 +6,8 @@ period = 5
 frequency = 1 / period
 amplitude = 3
 time = np.linspace(0, 15, 150)
-signal_square = amplitude * sg.sawtooth(2 * np.pi * frequency * time, width=0.5)
-plt.plot(time, signal_square)
+signal_triangle = amplitude * sg.sawtooth(2 * np.pi * frequency * time, width=0.5)
+plt.plot(time, signal_triangle)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.suptitle('Triangle Wave')
