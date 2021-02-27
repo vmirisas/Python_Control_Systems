6ada0a16c49cac2171dcfd83aa48c0abee81e5a2
Vasilis Mirisas <vmirisas@gmail.com>
2021-02-27 22:45:17 +0200

    corrections lab6 excercise1 corrections

--- lab6/exercises/exercise1/exercise1.py
+++ lab6/exercises/exercise1/exercise1.py
@@ -3,23 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 s = co.tf('s')
 """Για οποιοδήποτε κ το σύστημα είναι ευσταθές"""
-#g = ((s + 1) * (s + 4)) / (s ** 2 + 5 * s + 6)
-
+plt.figure(1)
+g1 = ((s + 1) * (s + 4)) / (s ** 2 + 5 * s + 6)
+co.root_locus(g1, grid=False)
 """Για περίπου κ=0.5 ο πραγματικός πόλος με του μιγαδικούς
  αλλάζουν μεριά, οπότε το σύστημα δεν είναι σταθεροποιήσιμο"""
-g= (s-2)/(s**3+2*s+1)
-#fg = co.feedback(0.5*g,sign=-1)
-#gpz = co.pzmap(fg)
+plt.figure(2)
+g2= (s-2)/(s**3+2*s+1)
+co.root_locus(g2, grid=False)
 
+plt.figure(3)
+g3 = (s-2)*((s**2)+2)/((s-3)*(s+1))
+rlist, klist = co.root_locus(g3,kvect=np.linspace(0.1,10,5001), grid=False)
 
-#g = (s-2)*((s**2)+2)/((s-3)*(s+1)) #????
-
-print(g)
-#gpz = co.pzmap(g)
-locus = co.root_locus(g)
-
-
-
-"""print(g_rlocus)
-print(klist)"""
 plt.show()
