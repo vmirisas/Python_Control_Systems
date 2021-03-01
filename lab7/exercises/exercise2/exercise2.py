76d722286e0e12412bf2a35095b083cee9e54e30
Vasilis Mirisas <vmirisas@gmail.com>
2021-03-01 17:31:14 +0200

    changes lab 7 code and figures

--- lab7/exercises/exercise2/exercise2.py
+++ lab7/exercises/exercise2/exercise2.py
@@ -1,22 +1,27 @@
 import control as co
-import numpy as np
 import matplotlib.pyplot as plt
-
 
 s = co.tf('s')
 g = (s + 1) / (s ** 3 + 6 * s - 5)
+
 plt.figure(1)
+siso = co.sisotool(g)
+
+plt.figure(2)
 co.pzmap(g)
-# siso = co.sisotool(g)
+
+# control function
 k = 11
 z = -1
 cs = k * (s - z)
 """Δοκιμάζοντας τιμές για μηδενικά, προσθέτω μόνο ένα πραγματικό μηδενικό, άρα το cs θα έχει 
 πολυώνυμο στο αριθμητή και 1 στον παρονομαστή"""
 
+# the controlled system
 g_feedback = co.feedback(co.series(g, cs), 1, sign=-1)
-plt.figure(2)
+print(g_feedback)
+
+# pole zero plot of the closed controlled system
+plt.figure(3)
 co.pzmap(g_feedback)
 plt.show()
-
-
--- lab7/exercises/exercise3/exercise3.py
+++ lab7/exercises/exercise3/exercise3.py
@@ -7,37 +7,59 @@ def os_cosines(x):
     log2 = (np.log(x)) ** 2
     pi2 = (np.pi) ** 2
     numrator = np.log(x)
-    denominator = np.sqrt(log2+pi2)
+    denominator = np.sqrt(log2 + pi2)
     return numrator / denominator
+
 
 def plot_line(slope):
     axes = plt.gca()
-    x_vals = np.array(axes.get_xlim())
-    y_vals = slope*x_vals
-    plt.plot(x_vals,y_vals, 'r--')
+    x1_vals = np.array(axes.get_xlim())
+    x_vals = np.array((x1_vals[0], 0))
+    y_vals = slope * x_vals
+    plt.plot(x_vals, y_vals, 'r--')
 
 
 s = co.tf('s')
-g = (2*s+1)/(2*s**3+4*s**2-8*s+1)
-#siso = co.sisotool(g)
+g = (2 * s + 1) / (2 * s ** 3 + 4 * s ** 2 - 8 * s + 1)
 
-k = 21.535
+plt.figure(1)
+siso = co.sisotool(g)
 
-"""βάζω ένα πόλο z=-1(+-)i"""
-cs = k*(1+s+(0.71*s)**2)
-g_feedback = co.feedback(co.series(g,cs),1,sign=-1)
-
+g_feedback = co.feedback(8 * g, 1, sign=-1)
+plt.figure(2)
 co.pzmap(g_feedback)
 
+# root locus plot
+plt.figure(3)
+co.root_locus(g, grid=False)
+
+# settling time plot
 TS = 7
+plt.axvline(-4 / TS, color='r', linestyle='--')
+
+# overshoot plots
 slope1 = np.tan(np.arccos(os_cosines(0.2)))
-plt.axvline(-4/TS, color='r', linestyle='--')
-
-
 plot_line(slope1)
 plot_line(-slope1)
 
 plt.xlim([-1.5, 0.5])
-plt.ylim([-1.3,1.3])
+plt.ylim([-5, 5])
+
+"""βάζω ένα πόλο z=-1(+-)i"""
+k = 21.535
+cs = k * (1 + s + (0.71 * s) ** 2)
+g_feedback = co.feedback(co.series(g, cs), 1, sign=-1)
+
+plt.figure(4)
+co.pzmap(g_feedback)
+
+TS = 7
+slope1 = np.tan(np.arccos(os_cosines(0.2)))
+plt.axvline(-4 / TS, color='r', linestyle='--')
+plot_line(slope1)
+plot_line(-slope1)
+
+plt.xlim([-1.5, 0.5])
+plt.ylim([-1.3, 1.3])
 
 plt.show()
