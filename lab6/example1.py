fe626deaaed26d97c3d7a8037cc4f96bc2d6a4c7
Vasilis Mirisas <vmirisas@gmail.com>
2021-05-31 21:52:37 +0300

    chapter 6 example1 corrections

--- lab6/example1.py
+++ lab6/example1.py
@@ -2,11 +2,20 @@ import control as co
 import matplotlib.pyplot as plt
 
 s = co.tf('s')
-#g = (s+4)/((s+2)*(s+1))
-#g = (s**2+3*s+4)/((s+2)*(s**2+1))
-g = (2*s +1)/(s**2 +4*s+8)
-g_rlocus = co.root_locus(g)
+g1 = (s + 4) / ((s + 2) * (s + 1))
+g2 = (s ** 2 + 3 * s + 4) / ((s + 2) * (s ** 2 + 1))
+g3 = (2 * s + 1) / (s ** 2 + 4 * s + 8)
 
+plt.figure(1)
+g1_rlocus = co.rlocus(g1)
+plt.xlim([-8, 0])
 
+plt.figure(2)
+g2_rlocus = co.rlocus(g2)
+plt.xlim([-2, 1])
+
+plt.figure(3)
+g3_rlocus = co.rlocus(g3)
+plt.xlim([-4, 0])
 
 plt.show()
